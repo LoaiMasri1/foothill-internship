4cebca4a39ac85956eb04fef2c837fa4852b0137
LoaiMasri1 <s12010098@stu.najah.edu>
2023-07-13 17:38:40 +0300

    add product model

---
+++ ims/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace ims.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
