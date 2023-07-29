199fc9939d3712fc795a248d4fcd1f326bd3a241
LoaiMasri1 <loaimasri2002@gmail.com>
2023-07-30 00:02:41 +0300

    add Result class
    result class is pattern to handle the errors that occurs in this system

---
+++ AirportTicket/Common/Error.cs
@@ -0,0 +1,18 @@
+﻿namespace AirportTicket.Common;
+
+public class Error
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Error.NullValue", "Value cannot be null");
+    public static readonly Error NotExpected = new("Error.NotExpected", "Error not expected");
+
+    public Error(string code, string errorMessage)
+    {
+        Code = code;
+        ErrorMessage = errorMessage;
+    }
+    public string Code { get; }
+    public string ErrorMessage { get; }
+    public DateTime Timestamp { get; } = DateTime.UtcNow;
+    public static implicit operator string(Error error) => $"{error.Code}: {error.ErrorMessage}";
+}
