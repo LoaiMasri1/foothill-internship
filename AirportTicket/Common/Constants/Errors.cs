88b1fec524cc520dbd0de37d4f83a2c8d3eddfda
LoaiMasri1 <loaimasri2002@gmail.com>
2023-07-30 00:36:28 +0300

    add flight possible errors

--- AirportTicket/Common/Constants/Errors.cs
+++ AirportTicket/Common/Constants/Errors.cs
@@ -24,4 +24,37 @@ public class Errors
         public static Error UserNotValid(string message) =>
             new("User.NotValid", message);
     }
+
+    public partial class Flight
+    {
+        public static readonly Error FlightAlreadyExists =
+        new("Flight.AlreadyExists", "Flight already exists");
+
+        public static readonly Error FlightNotFound =
+        new("Flight.NotFound", "Flight not found");
+
+        public static readonly Error FlightNotCreated =
+        new("Flight.NotCreated", "Flight not created");
+
+        public static readonly Error FlightNotUpdated =
+        new("Flight.NotUpdated", "Flight not updated");
+
+        public static readonly Error NoAvailableSeats =
+        new("Flight.NoAvailableSeats", "No available seats");
+
+        public static Error FlightNotValid(string message) =>
+        new("Flight.NotValid", message);
+    }
+
+    public partial class Deprature
+    {
+        public static Error DepratureNotValid(string message) =>
+        new("Deprature.NotValid", message);
+    }
+
+    public partial class Destination
+    {
+        public static Error DestinationNotValid(string message) =>
+        new("Destination.NotValid", message);
+    }
 }
