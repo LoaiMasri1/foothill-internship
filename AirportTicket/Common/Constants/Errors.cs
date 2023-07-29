55402529f6201ebf100f008b0ccb84673b89fc4c
LoaiMasri1 <loaimasri2002@gmail.com>
2023-07-30 00:08:05 +0300

    add error constants for csv importer

---
+++ AirportTicket/Common/Constants/Errors.cs
@@ -0,0 +1,10 @@
+﻿namespace AirportTicket.Common.Constants;
+
+public class Errors
+{
+    public partial class CSV
+    {
+        public static Error CSVNotValid(string message) =>
+        new("CSV.NotValid", message);
+    }
+}
