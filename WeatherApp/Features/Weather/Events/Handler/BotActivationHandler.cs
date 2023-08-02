f320bf0764ae2e36920409851abd52db568e1d96
LoaiMasri1 <loaimasri2002@gmail.com>
2023-08-02 20:35:48 +0300

    create handler for weatherData received event

---
+++ WeatherApp/Features/Weather/Events/Handler/BotActivationHandler.cs
@@ -0,0 +1,26 @@
+﻿using WeatherApp.Features.Weather.Events;
+using WeatherApp.Features.Weather.Factories;
+using WeatherApp.Features.Weather.Models;
+
+namespace WeatherApp.Features.Weather.Events.Handler;
+
+public class BotActivationHandler : IHandler<WeatherDataEventArgs>
+{
+    private readonly List<WeatherBotStrategy> _bots = new();
+    private readonly IWeatherBotFactory _factory;
+
+    public BotActivationHandler(IWeatherBotFactory factory)
+    {
+        _factory = factory;
+        _bots = _factory.GetBots();
+    }
+
+    public void Handle(object? sender, WeatherDataEventArgs e)
+    {
+
+        foreach (var bot in _bots)
+        {
+            bot.Activate(e.WeatherData);
+        }
+    }
+}
