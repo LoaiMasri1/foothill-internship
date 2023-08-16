4e6f91766b1b450c62ab1f421ddd0e8806293a9a
LoaiMasri1 <loaimasri2002@gmail.com>
2023-08-16 12:31:09 +0300

    init test app for airport ticket

---
+++ AirportTikcet.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AirportTikcet.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ AirportTikcet.Test/Usings.cs
@@ -0,0 +1,6 @@
+global using Xunit;
+global using AutoFixture;
+global using AutoFixture.Xunit2;
+global using Moq;
+global using AutoFixture.AutoMoq;
+global using System;
