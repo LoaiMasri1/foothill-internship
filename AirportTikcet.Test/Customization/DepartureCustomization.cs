0d20b0cf38d668e05eb3ec3c9b0f858e39957983
LoaiMasri1 <loaimasri2002@gmail.com>
2023-08-18 00:22:39 +0300

    add customization for booking,departure,user to get know fixture how to generate the data

---
+++ AirportTikcet.Test/Customization/DepartureCustomization.cs
@@ -0,0 +1,16 @@
+﻿using AirportTicket.Features.Flights.Models;
+
+namespace AirportTikcet.Test.Customization;
+
+public class DepartureCustomization : ICustomization
+{
+    public void Customize(IFixture fixture)
+    {
+        var departureDate = DateTime.Now.AddDays(1);
+        var flight = fixture.Build<Departure>()
+                            .With(d => d.Date, departureDate)
+                            .Create();
+
+        fixture.Inject(flight);
+    }
+}
