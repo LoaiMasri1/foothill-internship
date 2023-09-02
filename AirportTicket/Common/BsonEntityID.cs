37a4e5e36e1ef853a9028d9a2465f4ac42282c5b
LoaiMasri <loaimasri2002@gmail.com>
2023-09-02 18:31:33 +0300

    add bson as id

---
+++ AirportTicket/Common/BsonEntityID.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace AirportTicket.Common;
+
+public class BsonEntityID
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+}
