41425caaa5dead7888f6a3e2e701d274796dec66
LoaiMasri <loaimasri2002@gmail.com>
2023-09-16 11:54:23 +0300

    add customer service
    create/update/delete functionality

---
+++ RestaurantReservation/RestaurantReservation/Services/CustomerService.cs
@@ -0,0 +1,71 @@
+﻿using RestaurantReservation.Contracts.Requests;
+using RestaurantReservation.Contracts.Responses;
+using RestaurantReservation.Db;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Services;
+
+public class CustomerService
+{
+    private readonly RestaurantReservationDbContext _context;
+
+    public CustomerService(RestaurantReservationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CustomerResponse> CreateCustomerAsync(CustomerRequest customerRequest)
+    {
+        var newCustomer = new Customer
+        {
+            FirstName = customerRequest.FirstName,
+            LastName = customerRequest.LastName,
+            Email = customerRequest.Email,
+            PhoneNumber = customerRequest.PhoneNumber,
+        };
+
+        await _context.Customers.AddAsync(newCustomer);
+        await _context.SaveChangesAsync();
+
+        var response = new CustomerResponse(
+            newCustomer.CustomerId,
+            newCustomer.FirstName,
+            newCustomer.LastName,
+            newCustomer.Email,
+            newCustomer.PhoneNumber
+            );
+
+        return response;
+    }
+
+    public async Task<CustomerResponse> UpdateCustomerAsync(int id, CustomerRequest customerRequest)
+    {
+        var customer = await _context.Customers.FindAsync(id)
+            ?? throw new NotFoundException($"Customer with id {id} does not exist");
+
+        customer.FirstName = customerRequest.FirstName;
+        customer.LastName = customerRequest.LastName;
+        customer.Email = customerRequest.Email;
+        customer.PhoneNumber = customerRequest.PhoneNumber;
+
+        await _context.SaveChangesAsync();
+
+        var response = new CustomerResponse(
+            customer.CustomerId,
+            customer.FirstName,
+            customer.LastName,
+            customer.Email,
+            customer.PhoneNumber);
+
+        return response;
+    }
+
+    public async Task DeleteCustomerAsync(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id)
+            ?? throw new NotFoundException($"Customer with id {id} does not exist");
+
+        _context.Customers.Remove(customer);
+        await _context.SaveChangesAsync();
+    }
+}
