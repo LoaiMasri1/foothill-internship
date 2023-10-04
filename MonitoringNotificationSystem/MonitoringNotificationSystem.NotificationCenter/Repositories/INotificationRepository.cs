a4a82cf8d74862574de1a760e2788eab3f4f78fa
LoaiMasri <loaimasri2002@gmail.com>
2023-10-04 21:02:45 +0300

    implement save in mongo

---
+++ MonitoringNotificationSystem/MonitoringNotificationSystem.NotificationCenter/Repositories/INotificationRepository.cs
@@ -0,0 +1,8 @@
+﻿using MonitoringNotificationSystem.Shared.Data;
+
+namespace MonitoringNotificationSystem.NotificationCenter.Repositories;
+
+public interface INotificationRepository
+{
+    Task SaveStatisticsAsync(ServerStatistics statistics);
+}
---
+++ MonitoringNotificationSystem/MonitoringNotificationSystem.NotificationCenter/Repositories/NotificationRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MonitoringNotificationSystem.Shared.Configurations;
+using MonitoringNotificationSystem.Shared.Data;
+
+namespace MonitoringNotificationSystem.NotificationCenter.Repositories;
+
+public class NotificationRepository : INotificationRepository
+{
+    private readonly IMongoDatabase _database;
+    private readonly IMongoCollection<ServerStatistics> _collection;
+    private const string collectionName = "Statistics";
+
+    public NotificationRepository(IOptions<MongoDBConfig> options)
+    {
+        var client = new MongoClient(options.Value.ConnectionString);
+        _database = client.GetDatabase(options.Value.DatabaseName);
+        _collection = _database.GetCollection<ServerStatistics>(collectionName);
+    }
+
+    public async Task SaveStatisticsAsync(ServerStatistics statistics) =>
+        await _collection.InsertOneAsync(statistics);
+}
