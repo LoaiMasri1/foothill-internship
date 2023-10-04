15a5e508f31547c866c66e675ba79b56135a542c
LoaiMasri <loaimasri2002@gmail.com>
2023-10-04 20:54:50 +0300

    configure appsettings sections
    inject them with right config class

--- MonitoringNotificationSystem/MonitoringNotificationSystem.NotificationCenter/Program.cs
+++ MonitoringNotificationSystem/MonitoringNotificationSystem.NotificationCenter/Program.cs
@@ -1,8 +1,20 @@
 using MonitoringNotificationSystem.NotificationCenter;
+using MonitoringNotificationSystem.NotificationCenter.Setups;
+using MonitoringNotificationSystem.Shared.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
+
+builder.Services.Configure<ServerStatisticsConfig>(
+    builder.Configuration.GetSection(nameof(ServerStatisticsConfig))
+);
+
+builder.Services.Configure<AnomalyDetectionConfig>(
+    builder.Configuration.GetSection(nameof(AnomalyDetectionConfig))
+);
+
+builder.Services.ConfigureOptions<MongoDBConfigSetup>();
 
 var app = builder.Build();
 
---
+++ MonitoringNotificationSystem/MonitoringNotificationSystem.NotificationCenter/Setups/MongoDBConfigSetup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using MonitoringNotificationSystem.Shared.Configurations;
+
+namespace MonitoringNotificationSystem.NotificationCenter.Setups;
+public class MongoDBConfigSetup : IConfigureOptions<MongoDBConfig>
+{
+    private readonly IConfiguration _configuration;
+    private const string _sectionName = "MongoDBConfig";
+
+    public MongoDBConfigSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(MongoDBConfig options)
+    {
+        options.ConnectionString = _configuration.GetConnectionString(_sectionName)!;
+
+        _configuration.GetSection(nameof(MongoDBConfig)).Bind(options);
+    }
+}
