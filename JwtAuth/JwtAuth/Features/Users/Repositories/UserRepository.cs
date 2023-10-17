187768bfcd9a647730cc7a4427a06ea7023aa6bd
LoaiMasri <loaimasri2002@gmail.com>
2023-10-17 13:52:43 +0300

    add users feature
    with protected route

---
+++ JwtAuth/JwtAuth/Features/Users/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace JwtAuth.Features.Users.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly List<User> _users =
+        new()
+        {
+            new()
+            {
+                Username = "test",
+                Password = "test@123",
+                Email = "test@test"
+            }
+        };
+
+    public User GetUser(string username, string password)
+    {
+        var user = _users.FirstOrDefault(x => x.Username == username && x.Password == password);
+
+        if (user is null)
+        {
+            throw new Exception("User or passaword is invalid");
+        }
+
+        return user;
+    }
+}
