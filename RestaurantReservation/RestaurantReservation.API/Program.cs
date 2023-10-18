0514920bdb851d970482de268166f751bb538645
LoaiMasri <loaimasri2002@gmail.com>
2023-10-18 14:22:38 +0300

    add base api project

---
+++ RestaurantReservation/RestaurantReservation.API/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddControllers();
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UsePathBase("/api");
+
+app.MapControllers();
+
+app.Run();
